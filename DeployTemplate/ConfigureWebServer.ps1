91d23618c00a8d0c71759257dabd4081424817d4
Brian Keller <Brian@VM10-CLIENT>
2017-04-28 15:28:16 -0400

    change DSC script

--- DeployTemplate/ConfigureWebServer.ps1
+++ DeployTemplate/ConfigureWebServer.ps1
@@ -1,245 +1,162 @@
 Configuration Main
 {
-  param (  
-  [string[]]$MachineName = 'localhost'
-  )
+    param (  
+        [string[]]$MachineName = 'localhost'
+    )
 
-  Node ($MachineName)
-  {
-	   
-     WindowsFeature WebServerRole
-
+    Node ($MachineName)
+    {   
+        WindowsFeature WebServerRole
         {
-
             Name = "Web-Server"
-
             Ensure = "Present"
-
-            }
-
+        }
 	 
         WindowsFeature WebAppDev
-
         {
-
             Name = "Web-App-Dev"
-
             Ensure = "Present"
-
-
             DependsOn = "[WindowsFeature]WebServerRole"
+        }
 
-            }
-
-	   WindowsFeature WebAspNet45
-
+	    WindowsFeature WebAspNet45
         {
-
             Name = "Web-Asp-Net45"
-
             Ensure = "Present"
-
             Source = $Source
-
             DependsOn = "[WindowsFeature]WebServerRole"
-
-            }
+        }
 
         WindowsFeature WebNetExt35
-
         {
-
             Name = "Web-Net-Ext"
-
             Ensure = "Present"
-
             DependsOn = "[WindowsFeature]WebServerRole"
+        }
 
-            }
-
-	   WindowsFeature WebNetExt45
-
+	    WindowsFeature WebNetExt45
         {
-
             Name = "Web-Net-Ext45"
-
             Ensure = "Present"
-
             DependsOn = "[WindowsFeature]WebServerRole"
+        }
 
-            }
+	    WindowsFeature WebFtpServer
+	    {
+		    Name = "Web-Ftp-Server"
+		    Ensure = "Present"
+            DependsOn = "[WindowsFeature]WebServerRole"
+	    }
 
-	  WindowsFeature WebFtpServer
-	  {
-		Name = "Web-Ftp-Server"
-
-		Ensure = "Present"
-
-        DependsOn = "[WindowsFeature]WebServerRole"
-	  
-	  }
-
-	  WindowsFeature WebMgmtCompat
-	  {
-		Name = "Web-Mgmt-Compat"
-
-		Ensure = "Present"
-
-        DependsOn = "[WindowsFeature]WebServerRole"
-	  
-	  }
+	    WindowsFeature WebMgmtCompat
+	    {
+		    Name = "Web-Mgmt-Compat"
+		    Ensure = "Present"
+            DependsOn = "[WindowsFeature]WebServerRole"
+	    }
 
         WindowsFeature WebISAPIExt
-
         {
-
             Name = "Web-ISAPI-Ext"
-
             Ensure = "Present"
-
-
             DependsOn = "[WindowsFeature]WebServerRole"
-
-            }
+        }
 
         WindowsFeature WebISAPIFilter
-
         {
-
             Name = "Web-ISAPI-Filter"
-
             Ensure = "Present"
-
- 
             DependsOn = "[WindowsFeature]WebServerRole"
-
-            }
+        }
 
         WindowsFeature WebLogLibraries
-
         {
-
             Name = "Web-Log-Libraries"
-
             Ensure = "Present"
-
             DependsOn = "[WindowsFeature]WebServerRole"
-
-            }
+        }
 
         WindowsFeature WebRequestMonitor
-
         {
-
             Name = "Web-Request-Monitor"
-
             Ensure = "Present"
-
             DependsOn = "[WindowsFeature]WebServerRole"
-
-            }
+        }
 
         WindowsFeature WebMgmtTools
-
         {
-
             Name = "Web-Mgmt-Tools"
-
             Ensure = "Present"
-
             DependsOn = "[WindowsFeature]WebServerRole"
-
-            }
+        }
 
         WindowsFeature WebMgmtConsole
-
         {
-
             Name = "Web-Mgmt-Console"
-
             Ensure = "Present"
-
             DependsOn = "[WindowsFeature]WebServerRole"
+        }
 
-            }
-
-	  WindowsFeature WAS
-
+	    WindowsFeature WAS
         {
-
             Name = "WAS"
-
             Ensure = "Present"
-
             DependsOn = "[WindowsFeature]WebServerRole"
+        }
 
-            }
-
-	  WindowsFeature WASProcessModel
-
+	    WindowsFeature WASProcessModel
         {
-
             Name = "WAS-Process-Model"
-
             Ensure = "Present"
-
             DependsOn = "[WindowsFeature]WebServerRole"
+        }
 
-            }
-
-	   WindowsFeature WASNetEnvironment
-
+	    WindowsFeature WASNetEnvironment
         {
-
             Name = "WAS-NET-Environment"
-
             Ensure = "Present"
-
             DependsOn = "[WindowsFeature]WebServerRole"
+        }
 
-            }
-
-	  WindowsFeature WASConfigAPIs
-
+	    WindowsFeature WASConfigAPIs
         {
-
             Name = "WAS-Config-APIs"
-
             Ensure = "Present"
-
             DependsOn = "[WindowsFeature]WebServerRole"
-
-            }
-
-   #script block to download WebPI MSI from the Azure storage blob
-    Script DownloadWebPIImage
-    {
-        GetScript = {
-            @{
-                Result = "WebPIInstall"
-            }
         }
-        TestScript = {
-            Test-Path "C:\WindowsAzure\wpilauncher.exe"
-        }
-        SetScript ={
-            $source = "http://go.microsoft.com/fwlink/?LinkId=255386"
-            $destination = "C:\WindowsAzure\wpilauncher.exe"
-            Invoke-WebRequest $source -OutFile $destination
+
+        #script block to download WebPI MSI from the Azure storage blob
+        Script DownloadWebPIImage
+        {
+            GetScript = 
+            {
+                @{
+                    Result = "WebPIInstall"
+                }
+            }
+            TestScript = 
+            {
+                Test-Path "C:\temp\wpilauncher.exe"
+            }
+            SetScript =
+            {
+                $source = "http://go.microsoft.com/fwlink/?LinkId=255386"
+                $destination = "C:\temp\wpilauncher.exe"
+                Invoke-WebRequest $source -OutFile $destination
        
+            }
         }
-    }
 
-    Package WebPi_Installation
+        Package WebPi_Installation
         {
             Ensure = "Present"
             Name = "Microsoft Web Platform Installer 5.0"
-            Path = "C:\WindowsAzure\wpilauncher.exe"
+            Path = "C:\temp\wpilauncher.exe"
             ProductId = '4D84C195-86F0-4B34-8FDE-4A17EB41306A'
             Arguments = ''
         }
 
-    Package WebDeploy_Installation
+        Package WebDeploy_Installation
         {
             Ensure = "Present"
             Name = "Microsoft Web Deploy 3.5"
@@ -249,8 +166,6 @@ Configuration Main
 			Arguments = "/install /products:WDeploy  /AcceptEula"
 			DependsOn = @("[Package]WebPi_Installation")
         }
-	    
   }
-
 }
 Main
